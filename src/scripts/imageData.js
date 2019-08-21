6e11d50b07bd084e715f062db20d2eb4113ed6e9
Christoffer Berg <cgbjensen@gmail.com>
2019-08-21 11:29:38 +0200

    WIP

--- src/scripts/imageData.js
+++ src/scripts/imageData.js
@@ -1,26 +1,26 @@
 export const data = [
   {
-    image: 'https://source.unsplash.com/3Sf_G9m0gcQ/1440x700',
-    thumb: 'https://source.unsplash.com/3Sf_G9m0gcQ/150x300',
-    title: 'First text <br /> after the break',
-    nextTitle: 'First image <br /> cool',
+    image: "https://source.unsplash.com/w-bPRl6xNPs/1440x900",
+    thumb: "https://source.unsplash.com/w-bPRl6xNPs/150x300",
+    title: "Timon Klauser <br /> after the break",
+    nextTitle: "First image <br /> cool"
   },
   {
-    image: 'https://source.unsplash.com/y-RjWd6Ol7A/1440x700',
-    thumb: 'https://source.unsplash.com/y-RjWd6Ol7A/150x300',
-    title: 'Second text <br /> after the break',
-    nextTitle: 'Second image <br /> cool',
+    image: "https://source.unsplash.com/jgPdY5mINdk/1440x900",
+    thumb: "https://source.unsplash.com/jgPdY5mINdk/150x300",
+    title: "Second text <br /> after the break",
+    nextTitle: "Second image <br /> cool"
   },
   {
-    image: 'https://source.unsplash.com/IhXrWDckZOQ/1440x700',
-    thumb: 'https://source.unsplash.com/IhXrWDckZOQ/150x300',
-    title: 'Third text <br /> after the break',
-    nextTitle: 'Third image <br /> cool',
+    image: "https://source.unsplash.com/3MAmj1ZKSZA/1440x900",
+    thumb: "https://source.unsplash.com/3MAmj1ZKSZA/150x300",
+    title: "Timon Klauser <br /> - Zürich, Switzerland",
+    nextTitle: "Third image <br /> cool"
   },
   {
-    image: 'https://source.unsplash.com/9RIFeVL73sk/1440x700',
-    thumb: 'https://source.unsplash.com/9RIFeVL73sk/150x300',
-    title: 'Fourth text <br /> after the break',
-    nextTitle: 'Fourth image <br /> cool',
-  },
-]
+    image: "https://source.unsplash.com/9RIFeVL73sk/1440x900",
+    thumb: "https://source.unsplash.com/9RIFeVL73sk/150x300",
+    title: "Fourth text <br /> after the break",
+    nextTitle: "Fourth image <br /> cool"
+  }
+];
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,114 +1,119 @@
-import { TimelineMax, TweenMax } from 'gsap'
-import { data } from './imageData'
+import { TimelineMax, TweenMax } from "gsap";
+import { data } from "./imageData";
 
-let activeIndex = 0
+let activeIndex = 0;
 
-document.addEventListener('DOMContentLoaded', () => {
-  const nextButton = document.querySelector('.next-item')
-  nextButton.addEventListener('click', nextItem)
-
-  populateInitialData()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const nextButton = document.querySelector(".next-item");
+  nextButton.addEventListener("click", nextItem);
+  populateInitialData();
+});
 
 const populateInitialData = () => {
-  const images = document.querySelectorAll('.item__image')
-  const buttonImages = document.querySelectorAll('.preview__image')
+  const images = document.querySelectorAll(".item__image");
+  const buttonImages = document.querySelectorAll(".preview__image");
+  const titles = document.querySelectorAll(".item__title-text");
+  const nextTitles = document.querySelectorAll(".next-item__title-text");
 
-  images[0].src = `${data[activeIndex].image}`
-  images[1].src = `${data[getNextIndex()].image}`
-  buttonImages[0].src = `${data[getNextIndex()].thumb}`
-  buttonImages[1].src = `${data[getNextIndex(1)].thumb}`
+  // Images
+  images[0].src = `${data[activeIndex].image}`;
+  images[1].src = `${data[getNextIndex()].image}`;
 
-  const titles = document.querySelectorAll('.item__title-text')
-  titles[0].innerHTML = data[activeIndex].title
-  titles[1].innerHTML = data[getNextIndex()].title
-}
+  // Button images
+  buttonImages[0].src = `${data[getNextIndex()].thumb}`;
+  buttonImages[1].src = `${data[getNextIndex(1)].thumb}`;
+
+  // Button titles
+  nextTitles[0].innerHTML = data[getNextIndex()].nextTitle;
+  nextTitles[1].innerHTML = data[getNextIndex(1)].nextTitle;
+
+  // Big image titles
+  titles[0].innerHTML = data[activeIndex].title;
+  titles[1].innerHTML = data[getNextIndex()].title;
+};
 
 const titleAnimation = new TimelineMax({ paused: true })
-  .to('.item__title', 0.8, { ease: Power2.easeOut, yPercent: -50 }, 0)
-  .to('.item__title-text--first', 0.5, { opacity: 0 }, 0)
-  .to('.item__title-text--next', 0.5, { opacity: 1 }, 0)
+  .to(".item__title-wrapper", 0.8, { ease: Power2.easeOut, yPercent: -50 }, 0)
+  .to(".item__title-text--first", 0.5, { opacity: 0 }, 0)
+  .to(".item__title-text--next", 0.5, { opacity: 1 }, 0)
 
-  .eventCallback('onComplete', () => {
-    titleAnimation.progress(0).pause()
+  .eventCallback("onComplete", () => {
+    titleAnimation.progress(0).pause();
 
-    const titles = document.querySelectorAll('.item__title-text')
-    titles[0].innerHTML = data[activeIndex].title
-    titles[1].innerHTML = data[getNextIndex()].title
-  })
+    const titles = document.querySelectorAll(".item__title-text");
+    titles[0].innerHTML = data[activeIndex].title;
+    titles[1].innerHTML = data[getNextIndex()].title;
+  });
 
 const imageAnimation = new TimelineMax({ paused: true })
-  .to('.item__image--first', 0.8, { ease: Power2.easeOut, opacity: 0 }, 0)
+  .to(".item__image--first", 0.8, { ease: Power2.easeOut, opacity: 0 }, 0)
   .fromTo(
-    '.item__image--next',
+    ".item__image--next",
     0.8,
-    { ease: Power2.easeOut, opacity: 0, scale: 1.2 },
+    { ease: Power2.easeOut, opacity: 0, scale: 1.35 },
     { ease: Power2.easeOut, opacity: 1, scale: 1 },
     0
   )
-  .eventCallback('onComplete', () => {
-    imageAnimation.progress(0).pause()
+  .eventCallback("onComplete", () => {
+    imageAnimation.progress(0).pause();
 
-    const images = document.querySelectorAll('.item__image')
-    images[0].src = data[activeIndex].image
-    images[1].src = data[getNextIndex()].image
-  })
+    const images = document.querySelectorAll(".item__image");
+    images[0].src = data[activeIndex].image;
+    images[1].src = data[getNextIndex()].image;
+  });
 
 const previewAnimation = new TimelineMax({ paused: true })
-  .to('.next-item__content', 0.6, { ease: Sine.easeOut, xPercent: 100 })
-  .to(
-    '.next-item__content',
-    0.8,
-    { ease: Sine.easeOut, xPercent: 0 },
-    'sliderClosed+=0.15'
-  )
-  .to('.preview__image--first', 1, { ease: Power2.easeOut, xPercent: 10 }, 0)
+  .to(".next-item__content", 0.5, { ease: Sine.easeOut, x: 150 })
+  .to(".next-item__content", 0.5, { ease: Sine.easeOut, x: 0 }, "sliderClosed+=0.15")
+  .to(".preview__image--first", 0.8, { ease: Power2.easeOut, xPercent: 25 }, 0)
   .fromTo(
-    '.preview__image--next',
-    0.8,
+    ".preview__image--next",
+    0.6,
     { ease: Sine.easeOut, xPercent: 100 },
     { ease: Sine.easeOut, xPercent: 0 },
-    'sliderClosed+=0.05'
+    "sliderClosed+=0.05"
   )
-  .eventCallback('onComplete', () => {
-    previewAnimation.progress(0).pause()
+  .to(".next-item__title-text--first", 0.4, { opacity: 0 }, "sliderClosed+=0.15")
+  .fromTo(".next-item__title-text--next", 0.5, { opacity: 0 }, { opacity: 1 }, "sliderClosed+=0.15")
+  .eventCallback("onComplete", () => {
+    previewAnimation.progress(0).pause();
 
-    const images = document.querySelectorAll('.preview__image')
-    images[0].src = data[getNextIndex()].thumb
-    images[1].src = data[getNextIndex(1)].thumb
-  })
+    const nextTitles = document.querySelectorAll(".next-item__title-text");
+    nextTitles[0].innerHTML = data[activeIndex].nextTitle;
+    nextTitles[1].innerHTML = data[getNextIndex()].nextTitle;
+
+    const images = document.querySelectorAll(".preview__image");
+    images[0].src = data[getNextIndex()].thumb;
+    images[1].src = data[getNextIndex(1)].thumb;
+  });
 
 const nextItem = () => {
-  console.log('next item clicked')
+  console.log("next item clicked");
 
-  if (
-    !titleAnimation.isActive() &&
-    !imageAnimation.isActive() &&
-    !previewAnimation.isActive()
-  ) {
-    activeIndex = getNextIndex()
-    titleAnimation.play()
-    imageAnimation.play()
-    previewAnimation.play()
+  if (!titleAnimation.isActive() && !imageAnimation.isActive() && !previewAnimation.isActive()) {
+    activeIndex = getNextIndex();
+    titleAnimation.play();
+    imageAnimation.play();
+    previewAnimation.play();
   }
-}
+};
 
 // Helpers
 const getNextIndex = (skipSteps = 0) => {
   const incrementIndex = () => {
     if (newIndex >= data.length - 1) {
-      newIndex = 0
+      newIndex = 0;
     } else {
-      newIndex = newIndex + 1
+      newIndex = newIndex + 1;
     }
-  }
+  };
 
-  let newIndex = activeIndex
-  incrementIndex()
+  let newIndex = activeIndex;
+  incrementIndex();
 
   for (let i = 0; i < skipSteps; i++) {
-    incrementIndex()
+    incrementIndex();
   }
 
-  return newIndex
-}
+  return newIndex;
+};
