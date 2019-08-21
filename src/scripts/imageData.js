6bc3467c6794d8007e1c20eff390675cf6931a7a
Christoffer Berg <cgbjensen@gmail.com>
2019-08-21 12:42:37 +0200

    added more images

--- src/scripts/imageData.js
+++ src/scripts/imageData.js
@@ -22,5 +22,17 @@ export const data = [
     thumb: "https://source.unsplash.com/9RIFeVL73sk/150x300",
     title: "Fourth text <br /> after the break",
     nextTitle: "Fourth image <br /> cool"
+  },
+  {
+    image: "https://source.unsplash.com/GfmIT_pseDQ/1440x900",
+    thumb: "https://source.unsplash.com/GfmIT_pseDQ/150x300",
+    title: "Fifth text <br /> after the break",
+    nextTitle: "Fifth image <br /> cool"
+  },
+  {
+    image: "https://source.unsplash.com/Ns216N3sCAA/1440x900",
+    thumb: "https://source.unsplash.com/Ns216N3sCAA/150x300",
+    title: "Sixth text <br /> after the break",
+    nextTitle: "Sixth image <br /> cool"
   }
 ];
