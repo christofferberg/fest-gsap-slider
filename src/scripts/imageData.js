69df00fa611becc774c8260c404a57a2a735a279
Christoffer Berg <cgbjensen@gmail.com>
2019-08-22 09:12:39 +0200

    updated next title text

--- src/scripts/imageData.js
+++ src/scripts/imageData.js
@@ -3,36 +3,36 @@ export const data = [
     image: "https://source.unsplash.com/jgPdY5mINdk/1440x900",
     thumb: "https://source.unsplash.com/jgPdY5mINdk/150x300",
     title: "<h2>Chris Malinao Burgett</h2> " + "<p>KY, Louisville, United States</p>",
-    nextTitle: "First image <br /> cool"
+    nextTitle: "First image <br /> preview"
   },
   {
     image: "https://source.unsplash.com/-EPTweMPILk/1440x900",
     thumb: "https://source.unsplash.com/-EPTweMPILk/150x300",
     title: "<h2>Chris Benson</h2> " + "<p>New York, United States</p>",
-    nextTitle: "Second image <br /> cool"
+    nextTitle: "Second image <br /> preview"
   },
   {
     image: "https://source.unsplash.com/3MAmj1ZKSZA/1440x900",
     thumb: "https://source.unsplash.com/3MAmj1ZKSZA/150x300",
     title: "<h2>Timon Klauser</h2> " + "<p>Zürich, Switzerland</p>",
-    nextTitle: "Third image <br /> cool"
+    nextTitle: "Third image <br /> preview"
   },
   {
     image: "https://source.unsplash.com/9RIFeVL73sk/1440x900",
     thumb: "https://source.unsplash.com/9RIFeVL73sk/150x300",
     title: "<h2>Luis Vaz </h2> " + "<p>Bab Doukkala, Marrakesh, Morocco</p>",
-    nextTitle: "Fourth image <br /> cool"
+    nextTitle: "Fourth image <br /> preview"
   },
   {
     image: "https://source.unsplash.com/j4i3-p98Mno/1440x900",
     thumb: "https://source.unsplash.com/j4i3-p98Mno/150x300",
     title: "<h2>This Guy</h2> " + "<p>KY, Louisville, United States</p>",
-    nextTitle: "Fifth image <br /> cool"
+    nextTitle: "Fifth image <br /> preview"
   },
   {
     image: "https://source.unsplash.com/MWf9zsw0O0U/1440x900",
     thumb: "https://source.unsplash.com/MWf9zsw0O0U/150x300",
     title: "<h2>Clem Onojeghuo</h2> " + "<p>Shoreditch, London, United Kingdom</p>",
-    nextTitle: "Sixth image <br /> cool"
+    nextTitle: "Sixth image <br /> preview"
   }
 ];
